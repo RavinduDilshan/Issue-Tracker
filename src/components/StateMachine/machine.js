63eecb7105390d0b873354b198e41cd15fff5b2b
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2020-10-27 15:39:12 +0530

    define state machine

---
+++ src/components/StateMachine/machine.js
@@ -0,0 +1,40 @@
+
+  export const fetchMachine = Machine({
+    id:"stmachine",
+    initial:"New",
+    states:{
+      New:{
+        on:{
+          start:{
+            target:"wip"
+          },
+          cancel:{
+            target:"canceled"
+          }
+        }
+      },
+      wip:{
+        on:{
+          complete:{
+            target:"completed"
+          },
+          holding:{
+            target:"hold"
+          },
+          cancel:{
+            target:"canceled"
+          }
+        }
+      },
+      completed:{},
+      hold:{
+        on:{
+          resume:{
+            target:"wip"
+          }
+        }
+      },
+      canceled:{}
+    }
+  });
+  
